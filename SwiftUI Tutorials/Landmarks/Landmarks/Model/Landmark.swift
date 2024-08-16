1632a7ebb3492992f225b4834e45f325a971804b
AHEAD94 <ulysses94@naver.com>
2024-08-17 00:38:22 +0900

    feat: json parser와 LandmarkRow 뷰 구현

---
+++ SwiftUI Tutorials/Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  landmark.swift
+//  Landmarks
+//
+//  Created by Ryu Dae-ha on 8/16/24.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String // private: Landmark 구조체의 사용 주체들은 이미지 자체만 관심이 있음
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
--- SwiftUI Tutorials/SwiftUI essentials/CreatingAndCombiningViews/Complete/README.md
+++
@@ -1,3 +0,0 @@
-# Completed project: Creating and combining views
-
-Explore the completed project for the [Creating and combining views](https://developer.apple.com/tutorials/swiftui/creating-and-combining-views) tutorial.
